13bc2e70370a97db9283986778d58c62af139e39
Luis E Mena <lemena86@gmail.com>
2018-02-26 16:43:27 -0300

    user signip and signin using firebase

--- ng-complete-guide/src/app/app.component.ts
+++ ng-complete-guide/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB-AKnByh3t-21SKi_Zk0z5z3tNvcg_g9w',
+      authDomain: 'ng-recipe-book-2d2ac.firebaseapp.com'
+    });
+  }
 }
